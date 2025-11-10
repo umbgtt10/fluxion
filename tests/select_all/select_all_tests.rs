a40e80cdc022512f4a9cd277b8998adcf21ef3c1
Umberto Gotti <umbertogotti1979@gmail.com>
2025-11-10 05:49:26 +0100

    refactor(select_all_tests): simplified. No longer using requiring external crates

--- tests/select_all/select_all_tests.rs
+++ tests/select_all/select_all_tests.rs
@@ -1,32 +1,30 @@
-use crate::select_all::common::{Order, assert, send};
-use fluxion::sequenced_channel::unbounded_channel;
+use futures::StreamExt;
 use futures::stream::select_all;
+use tokio::sync::mpsc::unbounded_channel;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 
-async fn select_all_template_test(order1: Order, order2: Order, order3: Order) {
+async fn select_all_template_test(channel1: usize, channel2: usize, channel3: usize) {
     // Arrange
-    let (person_sender, person_receiver) = unbounded_channel();
-    let (animal_sender, animal_receiver) = unbounded_channel();
-    let (plant_sender, plant_receiver) = unbounded_channel();
+    let (tx1, rx1) = unbounded_channel();
+    let (tx2, rx2) = unbounded_channel();
+    let (tx3, rx3) = unbounded_channel();
+    let senders = [tx1, tx2, tx3];
 
-    let person_stream = UnboundedReceiverStream::new(person_receiver.into_inner());
-    let animal_stream = UnboundedReceiverStream::new(animal_receiver.into_inner());
-    let plant_stream = UnboundedReceiverStream::new(plant_receiver.into_inner());
+    let stream1 = UnboundedReceiverStream::new(rx1);
+    let stream2 = UnboundedReceiverStream::new(rx2);
+    let stream3 = UnboundedReceiverStream::new(rx3);
 
-    let senders = vec![person_sender, animal_sender, plant_sender];
-    let streams = vec![person_stream, animal_stream, plant_stream];
+    let mut merged = select_all(vec![stream1, stream2, stream3]);
 
-    let results = select_all(streams);
+    // Act
+    senders[channel1].send("First message").unwrap();
+    senders[channel2].send("Second message").unwrap();
+    senders[channel3].send("Third message").unwrap();
 
-    send(order1.clone(), senders.clone());
-    send(order2.clone(), senders.clone());
-    send(order3.clone(), senders.clone());
-
-    let mut results = Box::pin(results);
-
-    assert(order1, &mut results).await;
-    assert(order2, &mut results).await;
-    assert(order3, &mut results).await;
+    // Assert
+    assert_eq!(merged.next().await.unwrap(), "First message");
+    assert_eq!(merged.next().await.unwrap(), "Second message");
+    assert_eq!(merged.next().await.unwrap(), "Third message");
 }
 
 #[should_panic]
@@ -39,10 +37,10 @@ async fn test_select_all_all_combinations() {
 
     */
 
-    select_all_template_test(Order::Person, Order::Animal, Order::Plant).await;
-    select_all_template_test(Order::Person, Order::Plant, Order::Animal).await;
-    select_all_template_test(Order::Plant, Order::Animal, Order::Person).await;
-    select_all_template_test(Order::Plant, Order::Person, Order::Animal).await;
-    select_all_template_test(Order::Animal, Order::Person, Order::Plant).await;
-    select_all_template_test(Order::Animal, Order::Plant, Order::Person).await;
+    select_all_template_test(0, 1, 2).await; // channel 0, 1, 2
+    select_all_template_test(0, 2, 1).await; // channel 0, 2, 1
+    select_all_template_test(2, 1, 0).await; // channel 2, 1, 0
+    select_all_template_test(2, 0, 1).await; // channel 2, 0, 1
+    select_all_template_test(1, 0, 2).await; // channel 1, 0, 2
+    select_all_template_test(1, 2, 0).await; // channel 1, 2, 0
 }
