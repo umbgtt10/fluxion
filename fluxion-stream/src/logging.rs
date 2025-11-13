eb630d34c21a8dbb36b49d12bcac23169979dce8
Umberto Gotti <umbertogotti1979@gmail.com>
2025-11-13 19:17:41 +0100

    stream+exec: export logging macros under 	racing feature via macro_rules wrappers so crate::error!/warn!/info! resolve consistently; verify tests with feature on

--- fluxion-stream/src/logging.rs
+++ fluxion-stream/src/logging.rs
@@ -1,7 +1,28 @@
 // Conditional logging shim: uses `tracing` when enabled, falls back to eprintln!/println!
 
 #[cfg(feature = "tracing")]
-pub use tracing::{error, info, warn};
+#[macro_export]
+macro_rules! error {
+    ($($arg:tt)*) => {{
+        tracing::error!($($arg)*);
+    }};
+}
+
+#[cfg(feature = "tracing")]
+#[macro_export]
+macro_rules! warn {
+    ($($arg:tt)*) => {{
+        tracing::warn!($($arg)*);
+    }};
+}
+
+#[cfg(feature = "tracing")]
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)*) => {{
+        tracing::info!($($arg)*);
+    }};
+}
 
 #[cfg(not(feature = "tracing"))]
 #[macro_export]
