85f48b9a8083a34afff90227baf49a153b30f0e9
Umberto Gotti <umberto.gotti@umbertogotti.dev>
2025-11-21 07:46:42 +0100

    docs: consolidate CHANGELOG and fix doc test

--- fluxion-stream/src/lib.rs
+++ fluxion-stream/src/lib.rs
@@ -438,8 +438,8 @@
 //!
 //! // Chain: filter positives, map to string
 //! let mut composed = FluxionStream::new(stream)
-//!     .filter_ordered(|n| *n > 0)
-//!     .map_ordered(|item| format!("Value: {}", *item));
+//!     .filter_ordered(|&n| n > 0)  // filter_ordered receives &T::Inner
+//!     .map_ordered(|seq| format!("Value: {}", seq.value));  // map_ordered receives T
 //!
 //! tx.send(Sequenced::new(-1)).unwrap();
 //! tx.send(Sequenced::new(5)).unwrap();
