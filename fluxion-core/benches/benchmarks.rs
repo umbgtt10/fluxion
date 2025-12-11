c69c061a9339cf54163d35e11c04b91d32d86959
Umberto Gotti <umberto.gotti@umbertogotti.dev>
2025-12-11 18:27:06 +0100

    feat: benchmark implemented

---
+++ fluxion-core/benches/benchmarks.rs
@@ -0,0 +1,11 @@
+﻿// Copyright 2025 Umberto Gotti <umberto.gotti@umbertogotti.dev>
+// Licensed under the Apache License, Version 2.0
+// http://www.apache.org/licenses/LICENSE-2.0
+
+use crate::subject_bench::bench_subject;
+use criterion::{criterion_group, criterion_main};
+
+mod subject_bench;
+
+criterion_group!(benches, bench_subject);
+criterion_main!(benches);
